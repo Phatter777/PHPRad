c5f0dd045c4a2a86bd2c34f5eb33fb8b6b0bcc8b
pat <Phatscub2589@gmail.com>
2021-02-24 09:50:46 +0700

    first commit

---
+++ helpers/Menu.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Menu Items
+ * All Project Menu
+ * @category  Menu List
+ */
+
+class Menu{
+	
+	
+	public static $navbartopleft = array(
+		array(
+			'path' => 'home', 
+			'label' => 'Home', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'category', 
+			'label' => 'Category', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'port', 
+			'label' => 'Port', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'users', 
+			'label' => 'Users', 
+			'icon' => ''
+		)
+	);
+
+	public static $navbarsideleft = array(
+		array(
+			'path' => 'home', 
+			'label' => 'Home', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'category', 
+			'label' => 'Category', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'port', 
+			'label' => 'Port', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'users', 
+			'label' => 'Users', 
+			'icon' => ''
+		)
+	);
+
+	
+	
+	public static $name = array();
+
+}
